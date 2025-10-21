677ce20bb4d8af9cb2f0712b63d69d6d13a64b0d
Simon-Eke <simon3eke@gmail.com>
2025-10-21 09:53:24 +0200

    Create Controller, views and dedicated layoutfile for dashboard

---
+++ Mvc/Noog-mvc/Noog-mvc/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Noog_mvc.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
