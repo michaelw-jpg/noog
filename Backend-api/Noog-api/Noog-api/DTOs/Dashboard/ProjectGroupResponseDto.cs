214c99dd21a4e51a66a0f71113b96a260a7d1cdd
Simon-Eke <simon3eke@gmail.com>
2025-10-28 16:49:14 +0100

    Tested the endpoints through MVC. Mocked Api Data + Hardcoded MVC data

---
+++ Backend-api/Noog-api/Noog-api/DTOs/Dashboard/ProjectGroupResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Noog_api.DTOs.Dashboard
+{
+    public class ProjectGroupResponseDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Backend-api/Noog-api/Noog-api/Services/Dashboard/DashboardService.cs
@@ -0,0 +1,78 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Noog_api.DTOs;
+using Noog_api.DTOs.BaseResponseDtos;
+using Noog_api.DTOs.Dashboard;
+using Noog_api.Mappers;
+
+namespace Noog_api.Services.Dashboard
+{
+    public class DashboardService : IDashboardService
+    {
+        private readonly DashboardDependenciesMocker _mocker = new DashboardDependenciesMocker();
+        // private readonly IUserService _userService;
+        // private readonly IRecentProjectActivityService _activityService;
+        // private readonly IProjectGroupService _pgService;
+
+        // ctor *Tab*
+        public async Task<BaseResponseDto<DashboardDataResponseDto>> GetDashboardDataAsync()
+        {
+            // var userId = User.GetUserId();
+            // var recentActivities = await _activityService.GetRecentActivitiesAsync(userId: userId);
+            // var userInfo = await _userService.GetUserInfoAsync(userId: userId);
+
+            // TODO - Discuss tables like users and groupprojects
+            // TODO - This responseDto isn't flat and won't probably reflect a model. 
+
+
+
+            var result = _mocker.GetMockedDashboardData();
+
+            var response = new BaseResponseDto<DashboardDataResponseDto>();
+
+            if (result == null)
+            {
+                response.StatusCode = Enums.StatusCodesEnum.NotFound;
+                response.Message = "Dashboard Data not found";
+
+                return await Task.FromResult(response);
+            }
+
+            // var dtoList = GenericMapper.ToDto<DashboardData, DashboardDataResponseDto>(result);
+            response.Data = result;
+            response.StatusCode = Enums.StatusCodesEnum.Success;
+            response.Message = "Dashboard Data loaded successfully";
+
+
+            return await Task.FromResult(response);
+        }
+
+        public async Task<BaseResponseDto<List<ProjectGroupResponseDto>>> GetDashboardUserProjectGroupsAsync()
+        {
+            // var userId = User.GetUserId();
+            // var result = await _pgService.GetAllProjectGroupsByUserAsync(userId: userId);
+
+            var result = _mocker.GetMockedGroupProjects();
+
+            var response = new BaseResponseDto<List<ProjectGroupResponseDto>>();
+
+            if (result == null || !result.Any())
+            {
+                response.Data = new List<ProjectGroupResponseDto>();
+                response.StatusCode = Enums.StatusCodesEnum.Success;
+                response.Message = "No ProjectGroups found";
+            }
+            else
+            {
+                // var dtoList = GenericMapper.ToDtoList<ProjectGroup, ProjectGroupResponseDto>(result);
+                var dtoList = result;
+
+                response.Data = dtoList;
+                response.StatusCode = Enums.StatusCodesEnum.Success;
+                response.Message = "ProjectGroups loaded successfully";
+            }
+
+            return await Task.FromResult(response);
+        }
+    }
+}
