f7e92d18169d6988d1dd6334e5d5f468cd281c3e
Oliver <Oliver.garderud@chasacademy.se>
2025-11-03 15:24:59 +0100

    Added prompt dict and enums

---
+++ Backend-api/Noog-api/Noog-api/Services/IServices/IOpenAiPromptService.cs
@@ -0,0 +1,10 @@
+﻿using Noog_api.Models.AssemblyAi;
+using static Noog_api.Services.OpenAiPromptService;
+
+namespace Noog_api.Services.IServices
+{
+    public interface IOpenAiPromptService
+    {
+        Task<string> CreatePromptAsync(PromptType type);
+    }
+}
