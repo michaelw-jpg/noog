22bfbcc0b2417784b79db56560cda8794f1c49f1
Simon-Eke <simon3eke@gmail.com>
2025-11-04 09:50:05 +0100

    Project Group model

--- Backend-api/Noog-api/Noog-api/Models/Application/ProjectGroup.cs
+++ Backend-api/Noog-api/Noog-api/Models/Application/ProjectGroup.cs
@@ -2,5 +2,17 @@
 {
     public class ProjectGroup
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public ICollection<ProjectGroupUser> GroupUsers { get; set; }
+        public ICollection<RecentGroupActivity> RecentGroupActivities { get; set; }
+        public Guid MeetingId { get; set; }
+        public GroupMeeting GroupMeeting { get; set; }
+        public Guid ChatId { get; set; }
+        public GroupChat GroupChat { get; set; }
+        public Guid StorageId { get; set; }
+        public GroupStorage GroupStorage { get; set; }
+
     }
 }
