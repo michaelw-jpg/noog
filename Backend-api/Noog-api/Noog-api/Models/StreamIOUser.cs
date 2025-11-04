1b77f3a7b13a856eca651d3301be47b908157c6d
ikariLain <matheus-torrico@outlook.com>
2025-11-04 09:05:45 +0100

    Add StreamIO model

---
+++ Backend-api/Noog-api/Noog-api/Models/StreamIOUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Noog_api.Models
+{
+    public class StreamIOUser
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string userImage { get; set; }
+        public string token { get; set; }
+    }
+}
