ebffa4ed7eb849c8ec4827716e211234632ab019
Oliver <Oliver.garderud@chasacademy.se>
2025-11-05 20:50:38 +0100

    Template stuff for group project dashboard

---
+++ Mvc/Noog-mvc/Noog-mvc/Controllers/ProjectGroupController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Noog_mvc.Models.ProjectGroup;
+using Noog_mvc.Services;
+
+namespace Noog_mvc.Controllers
+{
+    public class ProjectGroupController : Controller
+    {
+        private readonly DashboardService _service;
+
+        public ProjectGroupController(DashboardService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var vm = new PGDashboardViewModel
+            {
+                //add logic and data
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ Mvc/Noog-mvc/Noog-mvc/Models/ProjectGroup/MeetingRoomViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Noog_mvc.Models.ProjectGroup
+{
+    public class MeetingRoomViewModel
+    {
+        public bool IsCallOngoing { get; set; }
+        public string Title { get; init; } = "Meeting room";
+
+        //Maybe add MeetingRoom id and Url, also timestamp?
+    }
+}
