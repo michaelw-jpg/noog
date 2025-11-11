ab7cb124afd67d3a7d2547bcde763653d2b15440
ikariLain <matheus-torrico@outlook.com>
2025-11-11 09:48:37 +0100

    Add Interface and Repository

---
+++ Backend-api/Noog-api/Noog-api/Repositories/IRepositories/IProjectGroupRepository.cs
@@ -0,0 +1,14 @@
+﻿using Noog_api.Models;
+using Noog_api.Models.Application;
+
+namespace Noog_api.Repositories.IRepositories
+{
+    public interface IProjectGroupRepository
+    {
+        Task<List<ProjectGroup>> GetAllGroupProjectAsync();
+        Task<ProjectGroup?> GetGroupProjectByIdAsync(int id);
+        Task<ProjectGroup> CreateGroupProjectsAsync(ProjectGroupRepository groupProject);
+        Task<ProjectGroup?> UpdateGroupProjectsAsync(int id, ProjectGroup updatedGroupProject);
+        Task<bool> DeleteGroupProjectAsync(int id);
+    }
+}
