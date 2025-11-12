f336dbf9f6bff37219d3cd702a2ceb5d2c715905
ikariLain <matheus-torrico@outlook.com>
2025-11-12 14:40:04 +0100

    Add User Dto, Patch and viewmodel

---
+++ Mvc/Noog-mvc/Noog-mvc/Models/User/UserPatchDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Noog_mvc.Models.User
+{
+    public class UserPatchDto
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
