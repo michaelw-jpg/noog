6303912a3a490ad2a31726a020734871a993a3d4
ikariLain <matheus-torrico@outlook.com>
2025-11-12 14:42:21 +0100

    Add ProfileConfig (draft) view, controller, service

---
+++ Mvc/Noog-mvc/Noog-mvc/Controllers/ProfileConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Noog_mvc.Services;
+using Noog_mvc.Models.User;
+
+namespace Noog_mvc.Controllers
+{
+    public class ProfileConfig : Controller
+    {
+        private readonly ProfileConfigService _service;
+
+        public async Task<IActionResult> Index()
+        {
+            //TODO: Implement ProfileConfigService and GetUserProfileAsync method
+            //var user = await _service.GetUserProfileByLogin();
+
+            var user = new UserViewModel
+            {
+                
+                Email = "meow",
+                FirstName = "John",
+                LastName = "Doe"
+                };
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> UpdateProfile(UserPatchDto userPatch)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _service.UpdateUserProfileAsync(userPatch);
+                if (result)
+                {
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError(string.Empty, "Failed to update profile.");
+            }
+            return View("Index", userPatch);
+        }
+
+
+    }
+}
