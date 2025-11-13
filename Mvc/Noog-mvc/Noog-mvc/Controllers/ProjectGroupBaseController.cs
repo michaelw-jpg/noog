bb070dee65526792c45b3b220e0a7cf1f8f1528b
Simon-Eke <simon3eke@gmail.com>
2025-11-13 14:04:08 +0100

    Summary styling completed. Tested flow with mocked data and still functional.

--- Mvc/Noog-mvc/Noog-mvc/Controllers/ProjectGroupBaseController.cs
+++ Mvc/Noog-mvc/Noog-mvc/Controllers/ProjectGroupBaseController.cs
@@ -18,6 +18,22 @@ namespace Noog_mvc.Controllers
                 ViewBag.ProjectGroupId = projectGroupId;
             }
 
+            /* === If testing and you wish for a projectgroupId
+            if (!context.RouteData.Values.TryGetValue("projectGroupId", out var pgId))
+            {
+                // Inject a mock projectGroupId for local testing
+                var testGuid = Guid.Parse("9b1deb4d-5b14-4886-9af0-1f7c3e0f3d00");
+                context.RouteData.Values["projectGroupId"] = testGuid;
+                ViewBag.ProjectGroupId = testGuid;
+            }
+            else if (Guid.TryParse(pgId?.ToString(), out var projectGroupId))
+            {
+                ViewBag.ProjectGroupId = projectGroupId;
+            }
+             
+             
+            */
+
             base.OnActionExecuting(context);
         }
     }
--- Mvc/Noog-mvc/Noog-mvc/ViewComponents/PGHeaderViewComponent.cs
+++ Mvc/Noog-mvc/Noog-mvc/ViewComponents/PGHeaderViewComponent.cs
@@ -13,11 +13,15 @@ namespace Noog_mvc.ViewComponents
         public async Task<IViewComponentResult> InvokeAsync(Guid projectGroupId)
         {
             // Hopeful real code
+            // Todo [Test if updates] - It might cache too good. And if so, remove the caching and let it 
             if(!_cache.TryGetValue($"group-{projectGroupId}", out TopSectionViewModel groupData))
             {
                 groupData = await _service.GetProjectGroupDataById(projectGroupId);
                 _cache.Set($"group-{projectGroupId}", groupData, TimeSpan.FromMinutes(10));
             }
+
+            // Pass along current context
+            ViewData["CurrentGroupId"] = projectGroupId;
 
             return View(groupData);
         }
