8cae97e331bbffb2f5f4956e179043285145c4b3
ikariLain <matheus-torrico@outlook.com>
2025-11-18 09:41:57 +0100

    Add Join call controller

---
+++ Backend-api/Noog-api/Noog-api/DTOs/StreamIODtos/JoinCallDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Noog_api.DTOs.StreamIODtos
+{
+    public class JoinCallDto
+    {
+        public string callId { get; set; }
+        public string userName { get; set; }
+        public string image { get; set; }
+        public string token { get; set; }
+        public string apikey { get; set; }
+    }
+}
