669f5fe3711f1a71193980de9183893981d57fad
OliverWG-net <oliver.garderud@chasacademy.se>
2025-11-21 18:35:46 +0100

    Added edit groupProject page and some styling.

---
+++ Mvc/Noog-mvc/Noog-mvc/Models/ProjectGroup/Dtos/ProjectGroupPatchDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Noog_mvc.Models.ProjectGroup.Dtos
+{
+    public class ProjectGroupPatchDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
